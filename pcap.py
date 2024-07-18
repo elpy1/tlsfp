3a1963e335f0229fc26c76e298feb76983275135
elpy <elpy@pm.me>
2024-07-19 01:05:04 +1000

    use dpkt for searching through pcap packet data

--- pcap.py
+++ pcap.py
@@ -1,43 +1,54 @@
-from tlsfp import client_hello_data, make_ja4, parse_tls_record
-from tls_vars import TLS_VERSIONS
 import argparse
+import dpkt
 import sys
+from tlsfp import client_hello_data, make_ja3, make_ja4, parse_tls_record
 
-PATTERN = b'\x16\x03'
 
 def parse_args():
-    """Parse CLI arguments"""
+    """CLI arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", type=str, required=True)
     return parser.parse_args()
 
-def get_all_records(pcap):
-    """Find and return all TLS client handshakes"""
-    records = []
-    versions = [int.to_bytes(v, 2) for v in TLS_VERSIONS]
-    with open(pcap, "rb") as f:
-        rest = f.read()
-        offset = -1
-        while (offset := rest.find(PATTERN, offset+1)) != -1:
-            if rest[offset+1:offset+3] not in versions:
-                continue
+
+def get_fingerprints(buf: bytes):
+    """Parse TLS client handshake data and return fingerprints"""
+    try:
+        record = parse_tls_record(buf)
+    except ValueError:
+        return
+
+    handshake = record.data
+    fp_data = client_hello_data(handshake.data)
+    return {
+        **make_ja3(fp_data),
+        **make_ja4(fp_data)
+    }
+
+
+def main(fname: str):
+    """Loop through pcap packets and print out JA4 TLS fingerprints"""
+    with open(fname, 'rb') as f:
+        pcap = dpkt.pcap.UniversalReader(f)
+        for _, buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
             try:
-                record = parse_tls_record(rest[offset:])
-            except ValueError:
+                ip = eth.data
+                tcp = ip.data
+                rest = tcp.data
+            except AttributeError:
                 continue
-            records.append(record)
-    return records
+
+            offset = -1
+            while (offset := rest.find(b'\x16\x03', offset + 1)) != -1:
+                if fp := get_fingerprints(rest[offset:]):
+                    print(fp.get('ja4'))
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    records = get_all_records(args.file)
-
-    if not records:
-        print('No records found.', file=sys.stderr)
+    try:
+        args = parse_args()
+        main(args.file)
+    except KeyboardInterrupt:
+        print('Keyboard interrupt. Exiting.', file=sys.stderr)
         sys.exit(1)
-
-    for rec in records:
-        fp_data = client_hello_data(rec.data.data)
-        print(make_ja4(fp_data).get('ja4'))
-
