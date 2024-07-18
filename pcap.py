0bfbe9241f9c37df8b907a5417cc7af0d2748164
elpy <elpy@pm.me>
2024-07-18 10:36:43 +1000

    use parse_tls_record function for pcap search

--- pcap.py
+++ pcap.py
@@ -1,8 +1,9 @@
-from tlsfp import parse_tls_handshake, client_hello_data, make_ja4, unpack_variable
+from tlsfp import client_hello_data, make_ja4, parse_tls_record
+from tls_vars import TLS_VERSIONS
 import argparse
 import sys
 
-PATTERN = b'\x16\x03\x01'
+PATTERN = b'\x16\x03'
 
 def parse_args():
     """Parse CLI arguments"""
@@ -10,18 +11,21 @@ def parse_args():
     parser.add_argument("--file", type=str, required=True)
     return parser.parse_args()
 
-
 def get_all_records(pcap):
     """Find and return all TLS client handshakes"""
     records = []
+    versions = [int.to_bytes(v, 2) for v in TLS_VERSIONS]
     with open(pcap, "rb") as f:
         rest = f.read()
-        p_len = len(PATTERN)
         offset = -1
-        while (offset := rest.find(PATTERN)) != -1:
-            record, rest = unpack_variable(16, rest[offset+p_len:])
-            if record[0] == 1:
-                records.append(record)
+        while (offset := rest.find(PATTERN, offset+1)) != -1:
+            if rest[offset+1:offset+3] not in versions:
+                continue
+            try:
+                record = parse_tls_record(rest[offset:])
+            except ValueError:
+                continue
+            records.append(record)
     return records
 
 
@@ -34,7 +38,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     for rec in records:
-        hs = parse_tls_handshake(rec)
-        fp_data = client_hello_data(hs.data)
+        fp_data = client_hello_data(rec.data.data)
         print(make_ja4(fp_data).get('ja4'))
 
